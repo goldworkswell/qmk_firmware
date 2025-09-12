93320f816107326b6638512c828b97ec50ae4deb
goldworkswell <goldworkswell@gmail.com>
2025-09-11 17:37:35 -0700

    works now

--- keyboards/handwired/dactyl_manuform/config.h
+++ keyboards/handwired/dactyl_manuform/config.h
@@ -23,8 +23,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 250
 #define DYNAMIC_TAPPING_TERM_INCREMENT 50
 
-#define SPLIT_HAND_PIN B3
-#define MASTER_RIGHT
+#define SPLIT_HAND_PIN F4
+
 
 
 
