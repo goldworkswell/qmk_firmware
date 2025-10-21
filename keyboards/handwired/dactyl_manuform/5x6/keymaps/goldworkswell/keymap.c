1ce7cf9e69ad83b5ed3d7d99df49a4f37ecf313c
goldworkswell <goldworkswell@gmail.com>
2025-10-21 14:28:43 -0700

    defines to make keys work and some lower layer changes

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/goldworkswell/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/goldworkswell/keymap.c
@@ -30,10 +30,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_LOWER] = LAYOUT_5x6(
-        QK_LAYER_LOCK,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-        QK_CAPS_WORD_TOGGLE,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
+        QK_LAYER_LOCK,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,S(KC_COMM),                        S(KC_DOT),KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
+        QK_CAPS_WORD_TOGGLE,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,KC_EQL,KC_PLUS,
         _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-        _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+        _______,_______,_______,_______,_______,S(KC_LBRC),                        S(KC_RBRC), KC_P1 , KC_P2 , KC_P3 ,_______ ,KC_UNDS,
                         _______,KC_PSCR,                                                            QK_BOOT, KC_P0,
                                                 _______,_______,            _______,_______,
                                               DT_UP,_______,            C(KC_Y),_______,
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           QK_LAYER_LOCK , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
           _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NUM,KC_INS ,KC_SCRL,KC_MUTE,
           _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_LEFT,KC_UP,KC_DOWN,KC_RIGHT,KC_VOLU,
-          _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,_______,_______,_______,KC_VOLD,
+          _______,_______,_______,_______,_______,S(KC_LBRC),                        S(KC_RBRC),_______,_______,_______,_______,KC_VOLD,
                          QK_BOOT,_______,                                  QK_DYNAMIC_MACRO_RECORD_START_1 ,QK_DYNAMIC_MACRO_RECORD_STOP,
                                                   _______,_______,            ARROW,_______,
                                                   _______,_______,            _______,_______,
